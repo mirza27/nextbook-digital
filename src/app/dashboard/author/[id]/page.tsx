ff7c121d921246c0b96f1c7cacd0ba2ff4cfb790
mirza27 <ramadhanimirza10@gmail.com>
2024-05-16 11:47:24 +0700

    udapet author page

---
+++ src/app/dashboard/author/[id]/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+import next from "next";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+const people = [
+  {
+    name: "Michael Foster",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  },
+  // More people...
+];
+
+export default function AuthorPage() {
+  return (
+    <>
+      <header className="mb-2.5">
+        <div>
+          <div>
+            <nav className="sm:hidden" aria-label="Back">
+              <a
+                href="#"
+                className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
+              >
+                <ChevronLeftIcon
+                  className="-ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400"
+                  aria-hidden="true"
+                />
+                Back
+              </a>
+            </nav>
+            <nav className="hidden sm:flex" aria-label="Breadcrumb">
+              <ol role="list" className="flex items-center space-x-4">
+                <li>
+                  <div className="flex">
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-gray-500 hover:text-gray-700"
+                    >
+                      Dashboard
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <ChevronRightIcon
+                      className="h-5 w-5 flex-shrink-0 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <a
+                      href="#"
+                      className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                    >
+                      Authors
+                    </a>
+                  </div>
+                </li>
+              </ol>
+            </nav>
+          </div>
+          <div className="mt-2 md:flex md:items-center md:justify-between">
+            <div className="min-w-0 flex-1">
+              <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                Authors
+              </h2>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="bg-white">
+        <div className="mx-auto max-w-7xl pt-2 pb-12 px-6 text-center lg:px-8 ">
+          <div className="space-y-8 sm:space-y-12">
+            <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+              <h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-gray-900">
+                All of the authors
+              </h2>
+              <p className="text-xl text-gray-500">
+                Risus velit condimentum vitae tincidunt tincidunt. Mauris
+                ridiculus fusce amet urna nunc. Ut nisl ornare diam in.
+              </p>
+            </div>
+            <ul
+              role="list"
+              className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6"
+            >
+              {people.map((person) => (
+                <li key={person.name}>
+                  <div className="space-y-4">
+                    <img
+                      className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                      src={person.imageUrl}
+                      alt=""
+                    />
+                    <div className="space-y-2">
+                      <div className="text-xs font-medium lg:text-sm">
+                        <h3>{person.name}</h3>
+                        <p className="text-indigo-600">{person.role}</p>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
